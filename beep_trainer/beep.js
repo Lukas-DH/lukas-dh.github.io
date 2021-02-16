b3be19a1b87fe9062053dbe6d4a28c036300fc74
Lukas Stojanov <git@lukas-stojanov.com>
2021-02-16 23:57:56 +0100

    fix +1 beep on stop buttom with clearTimer()

--- beep_trainer/beep.js
+++ beep_trainer/beep.js
@@ -1,32 +1,7 @@
 var audio = new Audio('beepsss.wav');
 var rand = 5
 var count = 0
-
-// var beep = function(){
-    //     // setInterval("console.log('wtf')", 100)
-    //     setInterval("audio.play()", 500)
-    //     // 
-    //     // let sound = document.getElementById("sound1");
-    //     // console.log("wtf")
-// }
-
-// var beep = setInterval(function (){
-//         // audio.play();
-//         rand = Math.round(Math.random()*(10000-5000))+5000; // generate new time (between 3sec and 500"s)
-//         console.log(rand)
-//     }, rand); 
-
-
-// function loop() {}
-
-// var beep = function(){
-//     var rand = Math.round(Math.random() * (6000 - 1000)) + 1000;
-//     setTimeout(function() {
-//         audio.play();
-//         beep();
-//         console.log(rand)
-//     }, rand);
-// };
+var timeoutID
 
 function counter() {
     count += 1
@@ -38,20 +13,28 @@ function main() {
     beep()
 }
 
+function playBeep() {
+    xmin = document.getElementById("myMin").value*1000
+    xmax = document.getElementById("myMax").value*1000
+    rand = Math.round(Math.random() * (xmax - xmin)) + xmin
+    timeoutID = setTimeout(function() {
+        audio.play();
+        console.log(rand)
+        beep()
+    } , rand)
+}
+
+function clearAlert() {
+    clearTimeout(timeoutID);
+  }
+
 var beep = function() {
     var change = document.getElementById("toggle")
     if (count % 2 == 1) {
         change.innerHTML = "Stop Beeps"
-        var rand = Math.round(Math.random() * (6000 - 1000)) + 1000;
-        setTimeout(function() {
-        audio.play();
-        beep();
-        console.log(rand)
-    }, rand);
-}
-        
+        playBeep()}
     if (count % 2 == 0) {
-        change.innerHTML = "Start Beeps"
+        change.innerHTML = "start Beeps"
+        window.clearTimeout(timeoutID)
     }
-
 }
